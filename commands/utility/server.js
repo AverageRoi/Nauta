8eb40f7b4da4088364d0c4710dc05b05b52e9dec
Rroix <averageworkfan@gmail.com>
2026-05-31 16:56:16 +0200

    Update server.js

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -3,7 +3,7 @@ const { execute } = require("./ping");
 const { SlashCommandBuilder } = require(discord.js);
 
 module.exports = {
-    data = new SlashCommandBuilder().setName("Server").setDescription("Provides info about the server"),
+    data: new SlashCommandBuilder().setName("Server").setDescription("Provides info about the server"),
     async execute(interaction) {
         await interaction.reply(
             "This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.",
