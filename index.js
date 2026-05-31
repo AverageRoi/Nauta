8001f8242d534c66671d3354e0412bb890888059
Rroix <averageworkfan@gmail.com>
2026-05-31 16:54:10 +0200

    s

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ for (const folder of commandsFolders) {
         const command = require(filePath);
         // Set a new item in the Collection with the key as the command name and the value as the exported module
         if ("data" in command && "execute" in command) {
-            client.command.set(command.data.name, command);
+            client.commands.set(command.data.name, command);
         } else {
             console.log("[WARNING] The command at ${filePath} is missing a required 'data' or 'execute' property.");
         }
