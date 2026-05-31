e8a6f5d3ab570d37fd6e50a746138663492b87d2
Rroix <averageworkfan@gmail.com>
2026-05-31 16:58:03 +0200

    Update server.js

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,6 +1,4 @@
-const { execute } = require("./ping");
-
-const { SlashCommandBuilder } = require(discord.js);
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder().setName("Server").setDescription("Provides info about the server"),
