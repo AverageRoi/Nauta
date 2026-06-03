d545ed07560263b50e0f56bbbd38d79934d2de26
Rroix <averageworkfan@gmail.com>
2026-06-03 23:42:21 +0200

    Separar las coordenadas en x, y, z y casos especiales

--- commands/utility/registrar.js
+++ commands/utility/registrar.js
@@ -25,8 +25,35 @@ module.exports = {
         ),
 
     async execute(interaction) {
-        const coordinates = interaction.options.getString("coordenadas");
+        const coordinates_from_user = interaction.options.getString("coordenadas");
         const dimension = interaction.options.getString("dimension");
+
+        const coordinates_untrimmed = coordinates_from_user.split(",")
+
+        // Declaro las variables, me acabo de enterar de que las variables declaradas dentro de ifs no persisten,
+        // pero los valores asignados dentro de ifs si.
+
+        let x_coordinates;
+        let y_coordinates;
+        let z_coordinates
+
+        // Para ver si no han introducido los datos necesarios
+        if (!coordinates_untrimmed[0] || !coordinates_untrimmed[1])  {
+            await interaction.reply( {content: "Please enter at least X and Y coordinates", flags: MessageFlags.Ephemeral });
+            return
+        }
+        // Para ver si sólo hay x e y
+        else if (!coordinates_untrimmed[2]) {
+            x_coordinates = coordinates_untrimmed[0].trim()
+            // Sólo por si acaso
+            y_coordinates = null
+            z_coordinates = coordinates_untrimmed[1].trim()
+        }
+        else {
+            x_coordinates = coordinates_untrimmed[0].trim()
+            y_coordinates = coordinates_untrimmed[1].trim()
+            z_coordinates = coordinates_untrimmed[2].trim()
+        }
 
         // Aquí iría la conexión con el prisma.js y todas esas cosiñas
         
