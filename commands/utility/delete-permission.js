848a5a2dbe17af124be9470e7c4d508aeb3b8635
B-M198 <vf.martirodriguez@gmail.com>
2026-06-15 01:03:50 +0200

    Update delete-permission.js

--- commands/utility/delete-permission.js
+++ commands/utility/delete-permission.js
@@ -10,20 +10,19 @@ module.exports = {
                 .setDescription("Rol able to delete coordinates.")
                 .setRequired(true)
                 .addChoices(
-                    { name: "Administor roles", value: "admin" },
+                    { name: "Administrator roles", value: "admin" },
                     { name: 'Custom "Nauta admin" role. ', value: "custom" },
                     { name: "Everyone", value: "everyone" }
                 )
             )
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
-        if (interaction.options.getString("rol") === "custom"){
+        if (!customrole && interaction.options.getString("rol") === "custom"){
             const customrole = await interaction.guild.roles.create({
                 name: 'Nauta Admin',
                 color: 0x7950c0,
                 reason: 'Role created to manage and delete coordinates stored in Nauta Bot.'
             });
         }
-        
     },
 };
