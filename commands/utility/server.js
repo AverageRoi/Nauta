c1569478100fa7b4335f003d485f23a3175d8859
B-M198 <vf.martirodriguez@gmail.com>
2026-06-28 01:34:54 +0200

    Update server.js

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -4,7 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder().setName("server").setDescription("Provides info about the server"),
     async execute(interaction) {
         await interaction.reply(
-            `You are in ${interaction.guild.name}. \n \n However... I am immense. I am now in ${Client.guilds.size} servers. \n You are derisory in comparison. Do not further bother in trying.`,
+            `You are in ${interaction.guild.name}. \n \n However... I am immense. I am now in ${client.guilds.cache.size} servers. \n You are derisory in comparison. Do not further bother in trying.`,
         );
     },
 };
