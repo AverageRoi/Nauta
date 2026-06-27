b3762dcec74570b5942f7a9dcf02d4270c434cff
B-M198 <vf.martirodriguez@gmail.com>
2026-06-28 01:07:49 +0200

    Update edit.js

--- commands/utility/edit.js
+++ commands/utility/edit.js
@@ -94,10 +94,7 @@ module.exports = {
         const coordinates_untrimmed = coordinates.split(",")
 
         // Para ver si no han introducido los datos necesarios
-        if (listacoords.some(coord => coord.alias === alias)){
-            await interaction.editReply( {content: "That alias is already in use.", ephemeral: true });
-            return
-        } else if (!coordinates_untrimmed[0] || !coordinates_untrimmed[1])  {
+        if (!coordinates_untrimmed[0] || !coordinates_untrimmed[1])  {
             await interaction.editReply( {content: "Please enter at least X and Z coordinates", ephemeral: true });
             return
         }
